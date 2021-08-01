db99928c3f502b139d681cdec25208d810405d05
wu_xx <wu_xx@ctrip.com>
2021-08-01 22:23:09 +0800

    update

--- 06/vue/src/main.js
+++ 06/vue/src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import routes from './router';
-
+import store from './store';
 
 Vue.config.productionTip = false;
 
@@ -21,6 +21,7 @@ function render(props = {}) {
 
   instance = new Vue({
     router,
+    store,
     render: h => h(App),
   }).$mount(container ? container.querySelector('#app') : '#app');
 }
